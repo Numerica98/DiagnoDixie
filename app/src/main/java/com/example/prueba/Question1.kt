402e339bd176e41ba54ce1fc2a39c5bbd224583e
Numerica98 <00041517@uca.edu.sv>
2019-06-23 19:15:09 -0600

    Agregada la actividad de la pregunta 1 y en blanco la pregunta 2

--- app/src/main/java/com/example/prueba/Question1.kt
+++ app/src/main/java/com/example/prueba/Question1.kt
@@ -1,5 +1,6 @@
 package com.example.prueba
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_question1.*
@@ -20,16 +21,107 @@ class Question1 : AppCompatActivity() {
         //Variable para tener acceso a la clase de las constantes
         var mApp = AppConstants()
 
-        tv_g_0_0.setOnClickListener {
+        //Variable para generar el intent mas adelante
+        val intentQuestion2 = Intent(this@Question1, Question2::class.java)
+
+        tv_question1_g_0_0.setOnClickListener {
             //Aumenta el contador cada vez que se presiona
             touchTextView1 = touchTextView1 +1
 
             //Verifica si es un numero impar para cambiar el color de seleccion
             if(touchTextView1 % 2 !=0){
-                tv_g_0_0.setBackgroundColor(resources.getColor(R.color.itemSelect))
+                tv_question1_g_0_0.setBackgroundColor(resources.getColor(R.color.itemSelect))
             }else {
               //En caso de ser par se coloca el color original
-                tv_g_0_0.setBackgroundColor(resources.getColor(R.color.colorWhite))
+                tv_question1_g_0_0.setBackgroundColor(resources.getColor(R.color.colorWhite))
+            }
+        }
+
+        tv_question1_p_0_1.setOnClickListener {
+            //Aumenta el contador cada vez que se presiona
+            touchTextView2 = touchTextView2 +1
+
+            //Verifica si es un numero impar para cambiar el color de seleccion
+            if(touchTextView2 % 2 !=0){
+                tv_question1_p_0_1.setBackgroundColor(resources.getColor(R.color.itemSelect))
+            }else {
+                //En caso de ser par se coloca el color original
+                tv_question1_p_0_1.setBackgroundColor(resources.getColor(R.color.colorWhite))
+            }
+        }
+
+        tv_question1_q_1_0.setOnClickListener {
+            //Aumenta el contador cada vez que se presiona
+            touchTextView3 = touchTextView3 +1
+
+            //Verifica si es un numero impar para cambiar el color de seleccion
+            if(touchTextView3 % 2 !=0){
+                tv_question1_q_1_0.setBackgroundColor(resources.getColor(R.color.itemSelect))
+            }else {
+                //En caso de ser par se coloca el color original
+                tv_question1_q_1_0.setBackgroundColor(resources.getColor(R.color.colorWhite))
+            }
+        }
+
+        tv_question1_p_1_1.setOnClickListener {
+            //Aumenta el contador cada vez que se presiona
+            touchTextView4 = touchTextView4 +1
+
+            //Verifica si es un numero impar para cambiar el color de seleccion
+            if(touchTextView4 % 2 !=0){
+                tv_question1_p_1_1.setBackgroundColor(resources.getColor(R.color.itemSelect))
+            }else {
+                //En caso de ser par se coloca el color original
+                tv_question1_p_1_1.setBackgroundColor(resources.getColor(R.color.colorWhite))
+            }
+        }
+
+        tv_question1_p_2_0.setOnClickListener {
+            //Aumenta el contador cada vez que se presiona
+            touchTextView5 = touchTextView5 +1
+
+            //Verifica si es un numero impar para cambiar el color de seleccion
+            if(touchTextView5 % 2 !=0){
+                tv_question1_p_2_0.setBackgroundColor(resources.getColor(R.color.itemSelect))
+            }else {
+                //En caso de ser par se coloca el color original
+                tv_question1_p_2_0.setBackgroundColor(resources.getColor(R.color.colorWhite))
+            }
+        }
+
+        tv_question1_g_2_1.setOnClickListener {
+            //Aumenta el contador cada vez que se presiona
+            touchTextView6 = touchTextView6 +1
+
+            //Verifica si es un numero impar para cambiar el color de seleccion
+            if(touchTextView6 % 2 !=0){
+                tv_question1_g_2_1.setBackgroundColor(resources.getColor(R.color.itemSelect))
+            }else {
+                //En caso de ser par se coloca el color original
+                tv_question1_g_2_1.setBackgroundColor(resources.getColor(R.color.colorWhite))
+            }
+        }
+
+        bt_Question1.setOnClickListener {
+            //Verifica que esten seleccionados los items correctos
+            if((touchTextView2 % 2 != 0 || touchTextView4 % 2 != 0 || touchTextView5 % 2 != 0) //Que solo uno de los 3 este seleccionado
+                    ||
+                    (touchTextView2 % 2 != 0 && touchTextView4 % 2 != 0 && touchTextView5 % 2 != 0) //Que todos los items correctos esten seleccionados
+                    ||
+                    (touchTextView2 % 2 != 0 && touchTextView4 % 2 != 0 ) //Que esten seleccionados 2 correctos
+                    ||
+                    (touchTextView2 % 2 != 0 && touchTextView5 % 2 != 0) //Que solo esten seleccionados 2 correctos
+                    ||
+                    (touchTextView4 % 2 != 0 && touchTextView5 % 2 != 0) //Que solo esten seleccionados 2 correctos
+            ){
+                //Aumenta el puntaje del nino
+                mApp.score = mApp.score +1
+
+                //Realiza el intent para ir a la segunda pregunta
+                startActivity(intentQuestion2)
+
+            }else {
+                startActivity(intentQuestion2)
             }
         }
     }
