efcaa60d32dea60e1389de899e8899945a666e14
Numerica98 <00041517@uca.edu.sv>
2019-06-24 08:18:31 -0600

    Agregados comentarios sobre la funcionalidad, pendiente de arreglar error de memoria

--- app/src/main/java/com/example/prueba/FinishActivity.kt
+++ app/src/main/java/com/example/prueba/FinishActivity.kt
@@ -13,17 +13,23 @@ class FinishActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_finish)
 
+        //Variable para acceder a la clase de las constantes
         var mApp = AppConstants()
 
+        //Variable para setear la imagen del personaje escogido, pendiente de arreglar
         //iv_characterSelected.setImageDrawable(mApp.getDrawable(mApp.characterKid))
 
+        //Accion del boton
         bt_NextResult.setOnClickListener {
+            //Confirma que el editText no venga vacio
             if(TextUtils.isEmpty(ed_PaswwordUserConfirm.text)){
                 Toast.makeText(this,"Ingrese el password para saber el resultado", Toast.LENGTH_LONG).show()
             }else{
+                //Verifica que sea el passwod correcto
                 if(ed_PaswwordUserConfirm.text.toString() != mApp.passwordUser){
                     Toast.makeText(this,"Ingrese el password correcto", Toast.LENGTH_LONG).show()
                 }else{
+                    //Pasa a la siguiente actividad
                     val intentResult = Intent(this@FinishActivity, ResultActivity::class.java)
                     startActivity(intentResult)
                 }
--- app/src/main/java/com/example/prueba/Question11.kt
+++ app/src/main/java/com/example/prueba/Question11.kt
@@ -12,18 +12,27 @@ class Question11 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_question11)
 
+        //Variable para tener acceso a la clase de las constantes
         var mApp = AppConstants()
 
+        //Accion del boton
         bt_Question11.setOnClickListener {
+            //Obtiene el radioButton selecionado
             var valor = rg_question11.checkedRadioButtonId
+
+            //Obtiene el id de los RadioButton pertenecientes al RadioGroup
             var radioButton = findViewById<RadioButton>(valor)
 
+            //Variable para realizar el intent mas adelante
             val intentFinish = Intent(this@Question11,FinishActivity::class.java)
 
+            //Verifica si el radioButton selecionado es el de la respuesta correcta
             if(radioButton == rb_question11_opc1){
+                //Aumenta el puntaje e inicia la siguiente actividad
                 mApp.score = mApp.score +1
                 startActivity(intentFinish)
             }else {
+                //Inicia la siguiente actividad
                 startActivity(intentFinish)
             }
         }
