0a1219d704e331b6dbafa477fbd92abf8ab24845
Numerica98 <00041517@uca.edu.sv>
2019-06-24 07:24:51 -0600

    Modificada la actividad final

--- app/src/main/java/com/example/prueba/ResultActivity.kt
+++ app/src/main/java/com/example/prueba/ResultActivity.kt
@@ -1,7 +1,9 @@
 package com.example.prueba
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_result.*
 
 class ResultActivity : AppCompatActivity() {
 
@@ -9,9 +11,30 @@ class ResultActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
 
+        //Variable para acceder a la constante
         var mApp = AppConstants()
 
+        //Coloca el valor del puntaje
+        tv_AnswerCountDetail.setText(mApp.score.toString())
 
+        //Dependiendo del puntaje obtenido en la prueba, coloca el diagnostico
+        if(mApp.score >= 2){
+            tv_RecomendationDetail.setText("El/la menor no presenta la condicion")
+        }else {
+            tv_RecomendationDetail.setText("El/la menor necesita una evaluacion para apoyar su proceso lector")
+        }
+
+        //Accion del boton
+        bt_NextFinish.setOnClickListener {
+            //Restaura los valores globales a su valor inicial
+            mApp.passwordUser = ""
+            mApp.characterKid = 0
+            mApp.score = 0
+
+            //Envia a la pantalla principal
+            val intentMain = Intent(this@ResultActivity, MainActivity::class.java)
+            startActivity(intentMain)
+        }
 
     }
 }
