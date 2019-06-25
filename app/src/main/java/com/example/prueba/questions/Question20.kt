2ad90de8f2903df08b97b92d8bb11c57cc5e5151
d1egopolanco <00301417@uca.edu.sv>
2019-06-24 18:26:44 -0600

    clases y xml preguntas

--- app/src/main/java/com/example/prueba/questions/Question20.kt
+++ app/src/main/java/com/example/prueba/questions/Question20.kt
@@ -1,28 +1,30 @@
 package com.example.prueba.questions
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.RadioButton
+import androidx.appcompat.app.AppCompatActivity
 import com.example.prueba.R
 import com.example.prueba.activities.FinishActivity
 import com.example.prueba.constants.AppConstants
-import kotlinx.android.synthetic.main.activity_question19.*
 
-class Question19 : AppCompatActivity() {
+import kotlinx.android.synthetic.main.activity_question20.*
+
+
+class Question20 : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_question19)
+        setContentView(R.layout.activity_question20)
 
-        bt_Question19.setOnClickListener {
-            var valor = rg_question19.checkedRadioButtonId
+        bt_Question20.setOnClickListener {
+            var valor = rg_question20.checkedRadioButtonId
 
             var radioButton = findViewById<RadioButton>(valor)
 
-            val intentFinish = Intent(this@Question19, Question20::class.java)
+            val intentFinish = Intent(this@Question20, FinishActivity::class.java)
 
-            if(radioButton == rb_question19_opc1){
+            if(radioButton == rb_question20_opc2){
                 AppConstants.score = AppConstants.score +1
                 startActivity(intentFinish)
             }else{
---
+++ app/src/main/java/com/example/prueba/questions/Question28.kt
@@ -0,0 +1,4 @@
+package com.example.prueba.questions
+
+class Question28 {
+}
